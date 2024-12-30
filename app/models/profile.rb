4bbee69c8192df343ca38b326da3d51c0070c729
Arish <thermic.arish@gmail.com>
2024-12-30 18:47:25 +0530

    Small Enhacement In View

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,16 @@
 class Profile < ApplicationRecord
   belongs_to :user
+
+  enum :gender, [:he_she, :him_her, :they_them, :other]
+
+  GENDER_DISPLAY = {
+    he_she: "He/She",
+    him_her: "Him/Her",
+    they_them: "They/Them",
+    other: "Other"
+  }.freeze
+
+  def gender_display
+    GENDER_DISPLAY[gender.to_sym]
+  end
 end
--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -5,7 +5,9 @@ module.exports = {
     './public/*.html',
     './app/helpers/**/*.rb',
     './app/javascript/**/*.js',
-    './app/views/**/*.{erb,haml,html,slim}'
+    './app/views/**/*.{erb,haml,html,slim}',
+    './app/assets/stylesheets/**/*.css',
+    './app/components/**/*.{erb,rb,html}'
   ],
   theme: {
     extend: {
