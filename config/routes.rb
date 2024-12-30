e14b4d5c90a1983a17d10353614a9175a76353c3
Arish <thermic.arish@gmail.com>
2024-12-30 15:43:57 +0530

    Signup Registration Complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+
   devise_for :users
+  get "dashboard", to: "dashboard#index"
+
   get "up" => "rails/health#show", as: :rails_health_check
   root "pages#index"
 end
