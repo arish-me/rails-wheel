4aa667ee10d83fb0702b65a41eb9b2220d385495
Arish <thermic.arish@gmail.com>
2025-05-29 11:49:56 +0530

    WIP Devise

--- app/helpers/settings_helper.rb
+++ app/helpers/settings_helper.rb
@@ -2,6 +2,8 @@ module SettingsHelper
   SETTINGS_ORDER = [
     { name: I18n.t("settings.settings_nav.profile_label"), path: :edit_settings_profile_path },
     { name: "Preferences", path: :settings_profile_preferences_path },
+    { name: "Account Setting", path: :settings_edit_account_path }
+
   ]
 
   def adjacent_setting(current_path, offset)
