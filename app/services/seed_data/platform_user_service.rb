7d1ea92121a6cbfd4d144287bf51ea67a4a89c8d
Arish <thermic.arish@gmail.com>
2025-06-28 11:51:46 +0530

    Complete adding platform user by service

--- app/services/seed_data/platform_user_service.rb
+++ app/services/seed_data/platform_user_service.rb
@@ -1,5 +1,5 @@
 module SeedData
-  class PlatformUserService
+  class PlatformUserService < BaseService
     def initialize(email)
       @email = email
     end
@@ -8,14 +8,15 @@ module SeedData
     end
 
     def seed_platform_user
-      service_user = User.find_or_initialize_by(email: @email) do |u|
+      log "Creating Plafrom user with email #{@email}"
+      user = User.find_or_initialize_by(email: @email) do |u|
         u.password = @email
         u.password_confirmation = @email
         u.user_type = 99
         u.onboarded_at = Time.now.utc
-        u.skip_confirmation = true
       end
-      service_user.confirm
+      user.skip_confirmation!
+      log "Finish Creating Plafrom user with email #{@email}"
     end
   end
 end
