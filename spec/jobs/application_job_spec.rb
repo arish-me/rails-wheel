e191b0894e358b3b8944f65234fe32a0b879c5c5
Arish <thermic.arish@gmail.com>
2025-06-28 18:04:46 +0530

    Mailers spec

---
+++ spec/jobs/application_job_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe ApplicationJob, type: :job do
+  it 'inherits from ActiveJob::Base' do
+    expect(ApplicationJob).to be < ActiveJob::Base
+  end
+
+  it 'can be instantiated' do
+    expect { ApplicationJob.new }.not_to raise_error
+  end
+
+  it 'raises NotImplementedError when performed' do
+    expect { ApplicationJob.perform_now }.to raise_error(NotImplementedError)
+  end
+end 
---
+++ spec/mailers/application_mailer_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe ApplicationMailer, type: :mailer do
+  it 'inherits from ActionMailer::Base' do
+    expect(ApplicationMailer).to be < ActionMailer::Base
+  end
+
+  it 'sets default from email' do
+    expect(ApplicationMailer.default[:from]).to eq(ENV.fetch("NO_REPLY_EMAIL") { "noreply@wheel.com" })
+  end
+
+  it 'can be instantiated' do
+    expect { ApplicationMailer.new }.not_to raise_error
+  end
+end 
