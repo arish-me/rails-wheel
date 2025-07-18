3bfffcc566b1bcb49fd525f5ceb5915fd85dd32f
Arish <thermic.arish@gmail.com>
2025-07-18 12:05:19 +0530

    Add Specialities and Skills View

---
+++ app/components/specialties_component.rb
@@ -0,0 +1,8 @@
+class SpecialtiesComponent  < ViewComponent::Base
+  private attr_reader :specialties, :title
+
+  def initialize(specialties, title)
+    @specialties = specialties
+    @title = title
+  end
+end
