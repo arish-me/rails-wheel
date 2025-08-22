e2021846bea5fd109b649346a81b974b192334be
Arish <thermic.arish@gmail.com>
2025-08-22 19:45:26 +0530

    Setting Nav for company and users

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -1,5 +1,5 @@
 class CandidatesController < ApplicationController
-  before_action :authenticate_user!, only: %i[edit update]
+  before_action :authenticate_user!
   before_action :set_candidate, only: %i[ show edit update ]
 
   def index
